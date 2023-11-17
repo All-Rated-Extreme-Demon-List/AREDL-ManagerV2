aa75346ad38c5e2ad2dc474d2509df50e8807858
Minebox260 <39033036+Minebox260@users.noreply.github.com>
2023-11-17 21:06:23 +0100

    Delete remove-commands.js

--- remove-commands.js
+++
@@ -1,9 +0,0 @@
-const { REST, Routes } = require('discord.js');
-const { clientId, token } = require('./config.json');
-
-const rest = new REST().setToken(token);
-
-// for guild-based commands
-rest.put(Routes.applicationGuildCommands(clientId, 379376350701551616), { body: [] })
-	.then(() => console.log('Successfully deleted all guild commands.'))
-	.catch(console.error);
