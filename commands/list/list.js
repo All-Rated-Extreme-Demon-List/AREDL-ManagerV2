c55e04ae936ad2beb9295290fe174a4516cad61b
Minebox260 <joellascoumes@gmail.com>
2024-09-08 22:20:05 +0200

    Mutual victors command

--- commands/list/list.js
+++ commands/list/list.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, AttachmentBuilder } = require('discord.js');
 const { githubOwner, githubRepo, githubDataPath, githubBranch } = require('../../config.json');
 
 module.exports = {
@@ -92,6 +92,20 @@ module.exports = {
 				.addStringOption(option =>
 					option.setName('newname')
 						.setDescription('The new name of the user')
+						.setRequired(true)))
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('mutualvictors')
+				.setDescription('Finds all victors that have beaten both levels')
+				.addStringOption(option =>
+					option.setName('level1')
+						.setDescription('The name of the first level')
+						.setAutocomplete(true)
+						.setRequired(true))
+				.addStringOption(option =>
+					option.setName('level2')
+						.setDescription('The name of the second level')
+						.setAutocomplete(true)
 						.setRequired(true))),
 	async autocomplete(interaction) {
 		const focused = interaction.options.getFocused();
@@ -484,6 +498,62 @@ module.exports = {
 			cache.updateUsers();
 			console.log(`${interaction.user.tag} (${interaction.user.id}) renamed ${user.name} (${user.user_id}) to ${newname}`);
 			return await interaction.editReply(`:white_check_mark: Successfully renamed **${user.name}** to **${newname}**`);
+
+		} else if (interaction.options.getSubcommand() === 'mutualvictors') {
+			const { cache, octokit } = require('../../index.js');
+			const { Op, Sequelize } = require('sequelize');
+
+			const level1 = interaction.options.getString('level1');
+			const level2 = interaction.options.getString('level2');
+
+			if (await cache.levels.findOne({ where: { filename: level1 } }) == null) return await interaction.editReply(`:x: Level **${level1}** not found`);
+			if (await cache.levels.findOne({ where: { filename: level2 } }) == null) return await interaction.editReply(`:x: Level **${level2}** not found`);
+
+
+			let level1_response, level2_response;	
+			try {
+				level1_response = await octokit.rest.repos.getContent({
+					owner: githubOwner,
+					repo: githubRepo,
+					path: githubDataPath + `/${level1}.json`,
+					branch: githubBranch,
+				});
+
+			} catch (fetchError) {
+				console.log(`Failed to fetch ${level1}.json: ${fetchError}`);
+				return await interaction.editReply(`:x: Failed to fetch data for **${level1}** from github; please try again later`);
+			}
+
+			try {
+				level2_response = await octokit.rest.repos.getContent({
+					owner: githubOwner,
+					repo: githubRepo,
+					path: githubDataPath + `/${level2}.json`,
+					branch: githubBranch,
+				});
+
+			} catch (fetchError) {
+				console.log(`Failed to fetch ${level2}.json: ${fetchError}`);
+				return await interaction.editReply(`:x: Failed to fetch data for **${level2}** from github; please try again later`);
+			}
+
+			const victors1 = JSON.parse(Buffer.from(level1_response.data.content, 'base64').toString('utf-8'))?.records;
+			const victors2 = JSON.parse(Buffer.from(level2_response.data.content, 'base64').toString('utf-8'))?.records;
+			
+
+			const mutualVictors = victors1.filter(victor1 => victors2.some(victor2 => victor2.user === victor1.user));
+			const mutualVictorNames = await cache.users.findAll({
+				where: {
+					user_id: {
+						[Op.in]: mutualVictors.map(victor => victor.user),
+					},
+				},
+				attributes: ['name'],
+			});
+
+			const mutualVictorNamesString = mutualVictorNames.map(victor => victor.name).join('\n- ');
+			const attachment = new AttachmentBuilder(Buffer.from("- " + mutualVictorNamesString)).setName(`mutual_victors_${level1}_${level2}.txt`);
+			return await interaction.editReply({ content: `:white_check_mark: Found ${mutualVictorNames.length} mutual victors between **${level1}** and **${level2}**\n`, files: [attachment] });
 		}
 	},
 };
